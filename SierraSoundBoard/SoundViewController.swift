ad88122218b2dd751d1fece2b1f40d22dd3f3848
neodeveloper8 <gonzaloast8@gmail.com>
2024-10-07 18:58:15 -0500

    segunda commit

---
+++ SierraSoundBoard/SoundViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SoundViewController.swift
+//  SierraSoundBoard
+//
+//  Created by Gonzalo Alonso Sierra Tapia on 7/10/24.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
